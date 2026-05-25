3a69f150af8e5fffefed894dc44beeb70b2246fe
vitorialisboacordeiro-hash <vitoria.lisboa.cordeiro@escola.pr.gov.br>
2026-05-25 10:46:17 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function mostrarMensagem() {
+    alert("O agronegócio sustentável ajuda a proteger o planeta e garantir um futuro melhor!");
+}
+
+function trocarCor() {
+    document.body.classList.toggle("dark-mode");
+}
